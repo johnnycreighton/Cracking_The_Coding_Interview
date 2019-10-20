23f69c9e6bfe95bdcf56ca8cf9f19cba4b4b27c5
Johnny Creighton <johnnycreighton88@hotmail.com>
2019-10-20 19:52:17 +0100

    Initial commit for master branch

---
+++ CrackingTheCodingInterview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrackingTheCodingInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
