6081e195bc8f41000430d60c2aa4ed3d2011d3ab
Johnny Creighton <johnnycreighton88@hotmail.com>
2019-11-06 22:32:38 +0000

    Refactoring classes to perform more logically.

--- CrackingTheCodingInterview/Program.cs
+++ CrackingTheCodingInterview/Program.cs
@@ -1,27 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using CrackingTheCodingInterview.Chapters.ChapterOne;
+using Newtonsoft.Json;
 
 namespace CrackingTheCodingInterview
 {
     class Program
     {
+        private static List<Question> Questions;
+        private static bool invalidInput = true;
+        private static int questionNumber = 0;
+
         static void Main()
         {
-            Console.WriteLine("****Cracking The Coding Interview****\n\n");
-            Console.WriteLine("Question 1.1:\nCheck to see if a string contains only unique characters, ie. one and only occurence of a char.\nInput the string you want to check.");
-            
+            LoadJson();
+            Console.WriteLine("****Welcome To Cracking The Coding Interview****\n\n");
+            Console.WriteLine("Which Question would you like to answer?");
+            Console.WriteLine("Please Choose a number:\n");
+
+            foreach(var question in Questions)
+            {
+                Console.WriteLine($"{question.id}: {question.desc}\n");
+            }
+
             while (true)
             {
-                var isUnique = Console.ReadLine();
-                var chapterOne = new QuestionOne_IsUnique();
+                GetQuestionNumberFromUser();
 
-                if (chapterOne.CheckUnique(isUnique)) 
-                    Console.WriteLine($"{isUnique} is a unique string.");
-                else
-                    Console.WriteLine($"{isUnique} is NOT a unique string.");
-                
-                Console.WriteLine("Press the any key to continue.");
-                Console.ReadKey();
+                switch (questionNumber)
+                {
+                    case 1:
+                        Console.WriteLine($"You have chosen Question: {questionNumber}.\n{Questions[questionNumber].desc}");
+                        QuestionOne_IsUnique.Run();
+                        break;
+                    case 2:
+                        Console.WriteLine($"You have chosen Question: {questionNumber}.\n{Questions[questionNumber].desc}");
+                        QuestionOne_IsUnique.Run();
+                        break;
+
+                    default:
+                        Console.WriteLine($"Question Number:{questionNumber} has not been implemented yet.");
+                        break;
+                }
+            }
+        }
+
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("Questions.json"))
+            {
+                string json = r.ReadToEnd();
+                Questions = JsonConvert.DeserializeObject<List<Question>>(json);
+            }
+        }
+
+        private static void GetQuestionNumberFromUser()
+        {
+            while (invalidInput)
+            {
+                try
+                {
+                    questionNumber = int.Parse(Console.ReadLine());
+                    invalidInput = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Please choose only the number.\nAny Other input will be ignored:");
+                }
             }
         }
     }
